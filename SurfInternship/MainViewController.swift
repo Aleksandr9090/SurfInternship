683ec4a52db9707c676045e0f1fb231b98803a5f
Aleksandr9090 <shutkin.90@mail.ru>
2023-02-03 10:11:41 +0400

    Initial Commit

---
+++ SurfInternship/MainViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MainViewController.swift
+//  SurfInternship
+//
+//  Created by Aleksandr on 03.02.2023.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+    }
+
+
+}
+
