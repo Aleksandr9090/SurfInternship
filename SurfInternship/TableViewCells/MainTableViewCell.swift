0fc110eb23587982031258fe74cd383ff90c7f61
Aleksandr Shutkin <Shutkin.90@mail.ru>
2023-02-12 18:52:17 +0400

    fix

--- SurfInternship/TableViewCells/MainTableViewCell.swift
+++ SurfInternship/TableViewCells/MainTableViewCell.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-// MARK: - FirstCollectionViewCellDelegate
+// MARK: - BubbleCollectionViewCellDelegate
 protocol BubbleCollectionViewCellDelegate {
     func cellDidTap(indexPath: IndexPath, isSelected: Bool, collectionViewIndex: Int)
 }
